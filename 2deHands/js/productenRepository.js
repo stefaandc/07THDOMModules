5e71e3e9c61aaca2f549771e72879984a939720b
Stefaan De Cock <stefaan.decock@hogent.be>
2022-05-03 10:46:44 +0200

    method uit repo verwijderd.

--- 2deHands/js/productenRepository.js
+++ 2deHands/js/productenRepository.js
@@ -10,10 +10,9 @@ export class ProductenRepository {
     return this.#producten;
   }
   voegProductToe(product) {}
-  geefProduct(id) {}
   geefProductenUitCategorie(categorie) {}
-  // retourneert een alfabetisch gesorteerde array van strings die de unieke categorieën bevat
   geefAlleCategorieen() {}
+
   haalProductenOp() {
     producten.forEach(
       ([
